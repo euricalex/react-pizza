e7891280d6cbd0af0be4e8bbacfbf9b13b776336
Alexander <122707746+euricalex@users.noreply.github.com>
2024-02-13 14:43:57 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App({title}) {
+  return <Greeting title = 'Hello World'/>
+    
 }
+export function Greeting({title}) {
+  const[word, setWord] = useState(title);
+  function handleClick() {
+    if(word === 'Hello World') {
+      setWord('Пошел нахуй!')
+    } else {
+      setWord('Hello World')
+    }
+  }
 
+  return <h1 onClick={handleClick}>{word}</h1>
+}
 export default App;
